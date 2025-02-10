83cffa06a773a35ecae632049a50eb911e209b91
Not5adija <khadija.elbedwehy@gmail.com>
2025-02-10 20:30:41 +0200

    Add files via upload

---
+++ basics/Khadija/task1.py
@@ -0,0 +1,4 @@
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+color=input("Enter your favorite color: ")
+print(f"Hello, {name}! You are {age} years old, and your favorite color is {color}.")
