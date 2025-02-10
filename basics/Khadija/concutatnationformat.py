a6122f338da463b80e4c9f13a9aba4cdbff426d9
Not5adija <khadija.elbedwehy@gmail.com>
2025-02-10 12:31:43 +0200

    Add files via upload

---
+++ basics/Khadija/concutatnationformat.py
@@ -0,0 +1,5 @@
+k=("khadija")
+name2=(' mohammed')
+age=  13 # it cant be int so we change into str
+print(k+name2+"13") # add space if you dont want them sticking to each other
+print(f"{k} {name2} {age}")# format to change from int to str
