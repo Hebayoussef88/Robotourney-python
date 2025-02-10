331519129cb1abaa606567f6e4908fcb37bf596e
78-bit <abdallag1772001@gmail.com>
2025-02-10 12:31:07 +0200

    Create conectation+format.py

---
+++ basics/Abdalla/conectation+format.py
@@ -0,0 +1,6 @@
+a =  'abdalla' #single quotian
+b = "gad"            #double quotian
+c  =  '''mohamed'''       #triple quotian
+age  = " i am 13 years old"           #int should be convertred to string
+print(a + b + c + age)
+print(f"{a} {b} {c} i am {age} years old")
