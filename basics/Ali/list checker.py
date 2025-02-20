c65791094b9c7deb8cae42887294f1775bfc1dc9
genetiX <127689965+genetix-gamedevelopment@users.noreply.github.com>
2025-02-20 20:58:24 +0200

    Add files via upload

---
+++ basics/Ali/list checker.py
@@ -0,0 +1,12 @@
+#list checker
+print("list checker")
+#This is the list
+list = ["ali", "zyad", "abdallah", "khadija"]
+#This is the name input
+name = str(input("your name?"))
+if name in list:
+    print(f"{name} is found on the list")
+else:
+    print(f"{name} is not found on the list")
+    print("too bad so sad!!")
+#This is the list checker
