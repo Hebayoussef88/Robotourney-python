5ed27b1b86627e0fabd4fc77e2511ae7c587f994
genetiX <127689965+genetix-gamedevelopment@users.noreply.github.com>
2025-03-05 21:28:40 +0200

    Update Game menu.py

--- game/Game menu.py
+++ game/Game menu.py
@@ -3,7 +3,7 @@ import os
 from pygame.display import get_window_size
 from pygame.locals import (K_w,K_s,K_d,K_a,K_ESCAPE,KEYDOWN,QUIT,)
 
-pygame.init
+pygame.init()
 
 screen_height = 672
 screen_width  = 1200
@@ -11,6 +11,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("future chronochills")
 screen1 = pygame.image.load("hack1 .jpg")
 scale = pygame.transform.scale(screen1, (800, 1200))
+Font1 = pygame.font.Font('Double Pixel-7 400.ttf', 135)
 
 running = True
 
@@ -21,7 +22,10 @@ while running:
         if event.type == QUIT:
             running = False
 
+    my_text = Font1.render("Future chronochills", True, (255, 255, 255))
+
     screen.blit(screen1, (0,0))
+    screen.blit(my_text, (180, 50))
     pygame.display.update()
 
 pygame.quit()
