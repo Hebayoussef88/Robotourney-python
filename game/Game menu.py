df70689a890ef3b260f04fcd97d6b740d15c8520
genetiX <127689965+genetix-gamedevelopment@users.noreply.github.com>
2025-03-05 16:45:37 +0200

    Update Game menu.py

--- game/Game menu.py
+++ game/Game menu.py
@@ -1,14 +1,16 @@
 import pygame
 import os
+from pygame.display import get_window_size
 from pygame.locals import (K_UP,K_DOWN,K_LEFT,K_RIGHT,K_ESCAPE,KEYDOWN,QUIT,)
 
 pygame.init
 
-screen_height = 800
+screen_height = 672
 screen_width  = 1200
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("future chronochills")
-screen1 = pygame.image.load("c:\Users\og126\Downloads\hack1.jpg")
+screen1 = pygame.image.load("hack1 .jpg")
+scale = pygame.transform.scale(screen1, (800, 1200))
 
 running = True
 
@@ -26,4 +28,5 @@ while running:
         elif event.type == QUIT:
             running = False
 
-
+    screen.blit(screen1, (0,0))
+    pygame.display.update()
