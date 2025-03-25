944dab8e2d5fe4872dd690b5baa3f4cededa4cc7
genetiX <127689965+genetix-gamedevelopment@users.noreply.github.com>
2025-03-25 22:54:40 +0200

    Add files via upload

---
+++ game/technology_block.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="technology" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="level1_bl.png" width="111" height="111"/>
+</tileset>
